69864a93d669e8aa3c1d7b3a80dcee9322e9eabc
Vivek Vasavada <vivek.vasavada@gmail.com>
2017-07-12 10:53:02 -0400

    added to lists

--- Listspractice/Program.cs
+++ Listspractice/Program.cs
@@ -10,6 +10,59 @@ namespace Listspractice
     {
         static void Main(string[] args)
         {
+            //List<int> favNumbers = new List<int>();
+            //favNumbers.Add(7);
+            //favNumbers.Add(25);
+            //favNumbers.Add(30);
+
+            //foreach(int number in favNumbers)
+            //{
+
+            //Console.WriteLine(number);
+            //}
+
+            //use the Count property
+            //Console.WriteLine(favNumbers.Count);
+
+            //List<string> books = new List<string> { "Cat in the Hat", "Toad and frog", "Green Eggs and Ham" };
+            //Console.WriteLine(books[0]);
+            //books.Insert(0, "Horton hears a Who");
+
+            //List<string> animals = new List<string>();
+            //animals.Add("Pig");
+            //animals.Add("Horse");
+            //animals.Add("Cow");
+            //animals.Add("Chicken");
+            //animals.Add("Dog");
+
+            //foreach (string animal in animals)
+            //{
+            //    Console.WriteLine(animal);
+
+            //}
+
+            //List<bool> boolList = new List<bool> { true, false, false, true, false };
+            //foreach (bool day in boolList)
+            //    if (day == true)
+            //    {
+
+            //        Console.WriteLine("Better bring an umbrella");
+
+            //    }
+            //else
+
+            //{
+            //        Console.WriteLine("No rain enjoy the sun");
+            //}
+            
+            List<string> favefoods = new List<string>() { "Steak", "Fish", "katsudon", "Ice Cream","Nachos" };
+            if (favefoods.Contains("fish"))
+            {
+                Console.WriteLine("i like fish, too");
+            }
+
+
         }
+        
     }
 }
